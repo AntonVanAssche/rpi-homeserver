c69a9dc141aadc13844f31ba03541d0d89c864d8
Anton Van Assche <vanasscheanton@gmail.com>
2021-08-28 21:26:58 +0200

    Remove deb file after install.

--- install-scripts/emby.sh
+++ install-scripts/emby.sh
@@ -5,3 +5,4 @@ EMBY_VERSION="4.6.4.0"
 message info "Installing emby-server..."
 wget https://github.com/MediaBrowser/Emby.Releases/releases/download/"$EMBY_VERSION"/emby-server-deb_"$EMBY_VERSION"_armhf.deb
 sudo apt install ./emby-server-deb_"$EMBY_VERSION"_armhf.deb -y
+rm -rf ./emby-server-deb_"$EMBY_VERSION"_armhf.deb
