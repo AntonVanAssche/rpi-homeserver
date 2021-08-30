5f3d64b83a30f8d0a70b1825eefb810c90d9c083
Anton Van Assche <vanasscheanton@gmail.com>
2021-08-30 17:38:30 +0200

    Correct LOG dir.

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ DIR="$HOME/rpi-homeserver"
 DATE=$(date +"%d-%m-%Y")
 TIME=$(date +"%T")
 
-LOG_DIR="$DOTFILES/log"
+LOG_DIR="$DIR/log"
 LOG_FILE="$LOG_DIR/$DATE-$TIME.log"
 
 # Create the LOG_DIR if no exists.
@@ -16,30 +16,30 @@ mkdir -p $LOG_DIR
 
 cd $HOME
 
-source ./rpi-homeserver/install-scripts/utils.sh
+source ./$DIR/install-scripts/utils.sh
 
 OS_CHECK
 
 function MAIN() {
 
-message quest "Press enter to begin installation (automatically install after 10s):"
-read -t10
+   message quest "Press enter to begin installation (automatically install after 10s):"
+   read -t10
 
-ASK_FOR_SUDO
+   ASK_FOR_SUDO
 
-source ./$DIR/install-scripts/packages.sh
+   source ./$DIR/install-scripts/packages.sh
 
-source ./$DIR/install-scripts/network.sh
+   source ./$DIR/install-scripts/network.sh
 
-source ./$DIR/install-scripts/emby.sh
+   source ./$DIR/install-scripts/emby.sh
 
-source ./$DIR/install-scripts/grafana.sh
+   source ./$DIR/install-scripts/grafana.sh
 
-source ./$DIR/install-scripts/transmission.sh
+   source ./$DIR/install-scripts/transmission.sh
 
-source ./$DIR/install-scripts/pihole.sh
+   source ./$DIR/install-scripts/pihole.sh
 
-source ./$DIR/install-scripts/pivpn.sh
+   source ./$DIR/install-scripts/pivpn.sh
 
 }
 
